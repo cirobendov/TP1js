fe8b4e3ae1d83fe583324f9d19f06c3908f82285
CIRO BEN DOV <48111117@ort>
2025-04-04 10:02:43 -0300

    ej06 hecho

--- ej06.js
+++ ej06.js
@@ -1,9 +1,30 @@
 function parsearUrl(laurl)
 {
 
-    let returnValue
-    
+    let miUrl    = null;
+    let miObjeto = null;
 
+    miUrl    = 'https://campus.ort.edu.ar/secundaria/almagro/informatica/tp/2217247/tp-3-node';
+    miObjeto = parsearUrl(miUrl);
 
+    console.log(miObjeto);
+
+    function parsearUrl(laURL) {
+        try {
+            const objeto = new URL(laURL);
+            return {
+                Host: objeto.host,
+                Pathname: objeto.pathname,
+                Parametros: objeto.searchParams.toString()
+            };
+        } catch (error) {
+            console.error("Error al parsear la URL:", error);
+            return { 
+                    Host: null, 
+                    Pathname: null, 
+                    Parametros: null 
+            };
+        }
+    }
 
 }
