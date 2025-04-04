9485ea4103d7191aa9c3a9e6fd6f40ef31d55c97
CIRO BEN DOV <48111117@ort>
2025-04-04 10:27:25 -0300

    ej07 hecho

---
+++ ej07.js
@@ -0,0 +1,20 @@
+import { countries } from 'countries-list';
+const { countries } = require('countries-list');
+
+let monedaDelPais, codigoPais;
+codigoPais = 'AR';
+monedaDelPais = obtenerMoneda(codigoPais);
+console.log(`La moneda del país ${codigoPais} es: ${monedaDelPais}`);
+
+codigoPais = 'UZA';
+monedaDelPais = obtenerMoneda(codigoPais);
+console.log(`La moneda del país ${codigoPais} es: ${monedaDelPais}`);
+
+function obtenerMoneda(codigoPais) {
+    if(countries[codigoPais]) {
+        return countries[codigoPais].currency;
+    }
+    return null;
+}
+
+
